e96c96285365d143283309dab5bc4bdd88e9fe2a
serenae45 <serena.emond1@gmail.com>
2023-10-27 15:16:57 -0400

    Update main.py

--- documents/draft/main.py
+++ documents/draft/main.py
@@ -1,2 +1,9 @@
 
 board = [[1,2,3],[4,5,6],[7,8,9]]
+direction = "up"
+curent_pos = [1][0]
+
+def move(board, curent_pos, direction):
+    if direction == "up":
+        #if board[curent_pos-1] does not give error, swap curent_letters
+    return board
