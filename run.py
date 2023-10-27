c67609e28a7cf24b55a2d14f5d1d734fd7cd7e1c
Ikra Mortoza <108554539+ikra-mortoza@users.noreply.github.com>
2023-10-27 17:05:33 -0400

    added more propositions and made assigned variables

--- run.py
+++ run.py
@@ -49,7 +49,7 @@ z = FancyPropositions("z")
 
 # slide puzzle propositions 
 @proposition(E)
-class assigned(num, pos): # assigns number to a position 
+class assigned(num, pos): # checks if number is assigned to a position 
     def __init__(self, num, pos):
         self.num = num
         self.pos = pos
@@ -65,6 +65,27 @@ class correct(num, pos): # checks if a number is at the right position
 
     def __str__(self) -> str:
         return f"({self.num} is at the correct position)"
+    
+@constraint.exactly_one(E) 
+@proposition(E) 
+class blank(i, j): # checks if a position is blank 
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+
+    def __str__(self) -> str:
+        return f"({self.i}, {self.j} is blank.)"
+
+
+# assign propositions to variables 
+board = [[1,2,3], [4,5,6], [7,8,"empty_box"]] # test input board 
+assigned_props = []
+
+for i in board:
+    for j in i: 
+        assigned_props.append(Assigned(j, [i,j])) # not sure if the indexing is right here 
+        
+    
 
 
 # Build an example full theory for your setting and return it.
@@ -87,7 +108,7 @@ def example_theory():
 
 # Create propositions for each position (i, j) that corresponds to the correct tile number
 n = 9
-tile_numbers = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]  
+tile_numbers = [[1, 2, 3], [4, 5, 6], [7, 8, "empty_box"]]  
 
 propositions = []
 
