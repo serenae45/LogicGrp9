58443f6cd32da998bd5544b69400403412ebfbb1
serenae45 <serena.emond1@gmail.com>
2023-10-27 16:43:30 -0400

    Added moves list and current position indexing
    moves list holds all of the moves made by the computer.
    current_pos holds the row and column number of the empty box in the board.

--- documents/draft/main.py
+++ documents/draft/main.py
@@ -1,42 +1,58 @@
 
-from operator import index
-from sre_compile import isstring
 
+board = [[4,5,6],
+         [7,8,1],
+         [2,3,"empty_box"]]
 
-board = [[1,2,3],[4,5,6],[7,8,"empty_box"]]
-winning_board = board = [[1,2,3],[4,5,6],[7,8,"empty_box"]]
-direction = "down"
-current_position = index("empty_box")
+winning_board = [[1,2,3],
+                 [4,5,6],
+                 [7,8,"empty_box"]]
+current_pos = [2, 2] # holds the row and column index's of the empty box in the board list.
+moves = []
 
-def swap(board, row, column, direction):
+def swap(board, current_pos, direction):
     if direction == "down":
-        if (row+1) >= 0 & (row+1)<= 3 & isstring(board[row][column]):
-            initial_pos = board[row][column] # holds the initial position number into a temporary variable
-            board[row][column] = board[row+1][column] # assign number in target position to initial position
-            board[row+1][column] = initial_pos # assign number in initial position to target position
-            return board
+        if (current_pos[0]+1) >= 0 & (current_pos[0]+1)<= 3:
+            initial_pos = board[current_pos[0]][current_pos[1]] # holds the initial position number into a temporary variable
+            board[current_pos[0]][current_pos[1]] = board[current_pos[0]+1][current_pos[1]] # assign number in target position to initial position
+            board[current_pos[0]+1][current_pos[1]] = initial_pos # assign number in initial position to target position
+            current_pos = [current_pos[0]+1,current_pos[1]] # update the current position to keep track of the empty box position
+            moves.append("down")
+            return board, current_pos
         return "Error: The current number is either in the bottom row and cannot swap, or the target position is not an empty box."
     if direction == "up":
-        if (row-1)>=0 & (row-1)<=3 & isstring(board[row][column]):
-            initial_pos = board[row][column] # holds the initial position number into a temporary variable
-            board[row][column] = board[row+1][column] # assign number in target position to initial position
-            board[row+1][column] = initial_pos # assign number in initial position to target position
-            return board
+        if (current_pos[0]-1)>=0 & (current_pos[0]-1)<=3:
+            initial_pos = board[current_pos[0]][current_pos[1]] # holds the initial position number into a temporary variable
+            board[current_pos[0]][current_pos[1]] = board[current_pos[0]-1][current_pos[1]] # assign number in target position to initial position
+            board[current_pos[0]-1][current_pos[1]] = initial_pos # assign number in initial position to target position
+            moves.append("up")
+            current_pos = [current_pos[0]-1,current_pos[1]] # update the current position to keep track of the empty box position
+            return board, current_pos
         return "Error: The current number is either at the top row and cannot swap up, or the target position is not an empty box."
     if direction == "right":
-        if (column+1)>=0 & (column+1)<=3 & isstring(board[row][column+1]):
-            initial_pos = board[row][column] # holds the initial position number into a temporary variable
-            board[row][column] = board[row][column+1] # assign number in target position to initial position
-            board[row][column+1] = initial_pos # assign number in initial position to target position
-            return board
+        if (current_pos[1]+1)>=0 & (current_pos[1]+1)<=3:
+            initial_pos = board[current_pos[0]][current_pos[1]] # holds the initial position number into a temporary variable
+            board[current_pos[0]][current_pos[1]] = board[current_pos[0]][current_pos[1]+1] # assign number in target position to initial position
+            board[current_pos[0]][current_pos[1]+1] = initial_pos # assign number in initial position to target position
+            current_pos = [current_pos[0],current_pos[1]+1] # update the current position to keep track of the empty box position
+            moves.append("right")
+            return board, current_pos
         return "Error: The current number is either in the right most position and cannot swap, or the target position is not an empty box."
     if direction == "left":
-        if (column-1)>=0 & (column-1)<=3 & isstring(board[row][column-1]):
-            initial_pos = board[row][column] # holds the initial position number into a temporary variable
-            board[row][column] = board[row][column-1] # assign number in target position to initial position
-            board[row][column-1] = initial_pos # assign number in initial position to target position
-            return board
+        if (current_pos[1]-1)>=0 & (current_pos[1]-1)<=3:
+            initial_pos = board[current_pos[0]][current_pos[1]] # holds the initial position number into a temporary variable
+            board[current_pos[0]][current_pos[1]] = board[current_pos[0]][current_pos[1]-1] # assign number in target position to initial position
+            board[current_pos[0]][current_pos[1]-1] = initial_pos # assign number in initial position to target position
+            current_pos = [current_pos[0],current_pos[1]-1] # update the current position to keep track of the empty box position
+            moves.append("left")
+            return board, current_pos
         return "Error: The current number is either in the left most position and cannot swap, or the target position is not an empty box"
+    if board == winning_board:
+        print("The slide puzzle has now been solved")
     return "Error: the direction entered is not an acceptible move."
 
-print(swap(board, 1, 2, direction))
+print(current_pos)
+board, current_pos = swap(board, current_pos, "up")
+print(board)
+print(current_pos)
+print("moves:", moves)
