09eba91f5b539d7163f8c54eb051fada3f9ee907
serenae45 <serena.emond1@gmail.com>
2023-10-27 16:07:13 -0400

    Changed a few things in the swap function

--- documents/draft/main.py
+++ documents/draft/main.py
@@ -1,36 +1,42 @@
 
+from operator import index
+from sre_compile import isstring
+
+
 board = [[1,2,3],[4,5,6],[7,8,"empty_box"]]
-direction = "right"
+winning_board = board = [[1,2,3],[4,5,6],[7,8,"empty_box"]]
+direction = "down"
+current_position = index("empty_box")
 
 def swap(board, row, column, direction):
     if direction == "down":
-        if (row+1) >= 0 & (row+1)<= 3:
+        if (row+1) >= 0 & (row+1)<= 3 & isstring(board[row][column]):
             initial_pos = board[row][column] # holds the initial position number into a temporary variable
             board[row][column] = board[row+1][column] # assign number in target position to initial position
             board[row+1][column] = initial_pos # assign number in initial position to target position
             return board
-        return "Error: The current number is in the top row, and there is no row above to swap."
+        return "Error: The current number is either in the bottom row and cannot swap, or the target position is not an empty box."
     if direction == "up":
-        if (row-1)>=0 & (row-1)<=3:
+        if (row-1)>=0 & (row-1)<=3 & isstring(board[row][column]):
             initial_pos = board[row][column] # holds the initial position number into a temporary variable
             board[row][column] = board[row+1][column] # assign number in target position to initial position
             board[row+1][column] = initial_pos # assign number in initial position to target position
             return board
-        return "Error: The current number is in the bottom row, and there is no row below to swap."
+        return "Error: The current number is either at the top row and cannot swap up, or the target position is not an empty box."
     if direction == "right":
-        if (column+1)>=0 & (column+1)<=3:
+        if (column+1)>=0 & (column+1)<=3 & isstring(board[row][column+1]):
             initial_pos = board[row][column] # holds the initial position number into a temporary variable
             board[row][column] = board[row][column+1] # assign number in target position to initial position
             board[row][column+1] = initial_pos # assign number in initial position to target position
             return board
-        return "Error: The current number is in the most right column, and there is no column to the right to swap."
+        return "Error: The current number is either in the right most position and cannot swap, or the target position is not an empty box."
     if direction == "left":
-        if (column-1)>=0 & (column-1)<=3:
+        if (column-1)>=0 & (column-1)<=3 & isstring(board[row][column-1]):
             initial_pos = board[row][column] # holds the initial position number into a temporary variable
             board[row][column] = board[row][column-1] # assign number in target position to initial position
             board[row][column-1] = initial_pos # assign number in initial position to target position
             return board
-        return "Error: The current number is in the most left column, and there is no column to the left to swap."
+        return "Error: The current number is either in the left most position and cannot swap, or the target position is not an empty box"
     return "Error: the direction entered is not an acceptible move."
 
 print(swap(board, 1, 2, direction))
